b8258aa77561b892ae6f070cfde79a1094b04b91
Samir Lazzali <19222100+samirLazzali@users.noreply.github.com>
2020-06-02 17:46:38 +0200

    Add cours 7
    Moved ipynb

--- 2.1_Introduction_to_Optimization_and_The_Efficient_Frontier/edhec_risk_kit.py
+++ 2.1_Introduction_to_Optimization_and_The_Efficient_Frontier/edhec_risk_kit.py
@@ -1,26 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def drawdown(return_series: pd.Series):
-    """Takes a time series of asset returns.
-       returns a DataFrame with columns for
-       the wealth index, 
-       the previous peaks, and 
-       the percentage drawdown
-    """
-    wealth_index = 1000*(1+return_series).cumprod()
-    previous_peaks = wealth_index.cummax()
-    drawdowns = (wealth_index - previous_peaks)/previous_peaks
-    return pd.DataFrame({"Wealth": wealth_index, 
-                         "Previous Peak": previous_peaks, 
-                         "Drawdown": drawdowns})
-
-
 def get_ffme_returns():
     """
     Load the Fama-French Dataset for the returns of the Top and Bottom Deciles by MarketCap
     """
-    me_m = pd.read_csv("../../data/Portfolios_Formed_on_ME_monthly_EW.csv",
+    me_m = pd.read_csv("../data/Portfolios_Formed_on_ME_monthly_EW.csv",
                        header=0, index_col=0, na_values=-99.99)
     rets = me_m[['Lo 10', 'Hi 10']]
     rets.columns = ['SmallCap', 'LargeCap']
@@ -33,11 +18,20 @@ def get_hfi_returns():
     """
     Load and format the EDHEC Hedge Fund Index Returns
     """
-    hfi = pd.read_csv("../../data/edhec-hedgefundindices.csv",
+    hfi = pd.read_csv("../data/edhec-hedgefundindices.csv",
                       header=0, index_col=0, parse_dates=True)
     hfi = hfi/100
     hfi.index = hfi.index.to_period('M')
     return hfi
+
+def get_ind_returns():
+    """
+    Load and format the Ken French 30 Industry Portfolios Value Weighted Monthly Returns
+    """
+    ind = pd.read_csv("../data/ind30_m_vw_rets.csv", header=0, index_col=0)/100
+    ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period('M')
+    ind.columns = ind.columns.str.strip()
+    return ind
 
 
 def skewness(r):
@@ -66,6 +60,40 @@ def kurtosis(r):
     return exp/sigma_r**4
 
 
+def annualize_rets(r, periods_per_year):
+    """
+    Annualizes a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    to the reader :-)
+    """
+    compounded_growth = (1+r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth**(periods_per_year/n_periods)-1
+
+
+def annualize_vol(r, periods_per_year):
+    """
+    Annualizes the vol of a set of returns
+    We should infer the periods per year
+    but that is currently left as an exercise
+    to the reader :-)
+    """
+    return r.std()*(periods_per_year**0.5)
+
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
+
+
 import scipy.stats
 def is_normal(r, level=0.01):
     """
@@ -79,14 +107,34 @@ def is_normal(r, level=0.01):
         statistic, p_value = scipy.stats.jarque_bera(r)
         return p_value > level
 
-    
+
+def drawdown(return_series: pd.Series):
+    """Takes a time series of asset returns.
+       returns a DataFrame with columns for
+       the wealth index, 
+       the previous peaks, and 
+       the percentage drawdown
+    """
+    wealth_index = 1000*(1+return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks)/previous_peaks
+    return pd.DataFrame({"Wealth": wealth_index, 
+                         "Previous Peak": previous_peaks, 
+                         "Drawdown": drawdowns})
+
+
 def semideviation(r):
     """
     Returns the semideviation aka negative semideviation of r
-    r must be a Series or a DataFrame
+    r must be a Series or a DataFrame, else raises a TypeError
     """
-    is_negative = r < 0
-    return r[is_negative].std(ddof=0)
+    if isinstance(r, pd.Series):
+        is_negative = r < 0
+        return r[is_negative].std(ddof=0)
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(semideviation)
+    else:
+        raise TypeError("Expected r to be a Series or DataFrame")
 
 
 def var_historic(r, level=5):
@@ -97,7 +145,6 @@ def var_historic(r, level=5):
     """
     if isinstance(r, pd.DataFrame):
         return r.aggregate(var_historic, level=level)
-    
     elif isinstance(r, pd.Series):
         return -np.percentile(r, level)
     else:
