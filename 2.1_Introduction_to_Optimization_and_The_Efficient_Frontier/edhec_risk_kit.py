578825b8e6ef09d9d13c8e06e7b8840075b87459
Samir Lazzali <19222100+samirLazzali@users.noreply.github.com>
2020-06-02 22:21:30 +0200

    ef2 p 3
    Build the minimzer to get efficent weight for several assets

--- 2.1_Introduction_to_Optimization_and_The_Efficient_Frontier/edhec_risk_kit.py
+++ 2.1_Introduction_to_Optimization_and_The_Efficient_Frontier/edhec_risk_kit.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import sys
 
 def get_ffme_returns():
     """
@@ -216,3 +215,51 @@ def plot_ef2(n_points, er, cov):
         "Volatility": vols
     })
     return ef.plot.line(x="Volatility", y="Returns", style=".-")
+
+
+from scipy.optimize import minimize
+
+def minimize_vol(target_return, er, cov):
+    """
+    Returns the optimal weights that achieve the target return
+    given a set of expected returns and a covariance matrix
+    """
+    n = er.shape[0]
+    init_guess = np.repeat(1/n, n)
+    bounds = ((0.0, 1.0),) * n # an N-tuple of 2-tuples!
+    # construct the constraints
+    weights_sum_to_1 = {'type': 'eq',
+                        'fun': lambda weights: np.sum(weights) - 1
+    }
+    return_is_target = {'type': 'eq',
+                        'args': (er,),
+                        'fun': lambda weights, er: target_return - portfolio_return(weights,er)
+    }
+    weights = minimize(portfolio_vol, init_guess,
+                       args=(cov,), method='SLSQP',
+                       options={'disp': False},
+                       constraints=(weights_sum_to_1,return_is_target),
+                       bounds=bounds)
+    return weights.x
+
+
+def optimal_weights(n_points, er, cov):
+    """
+    """
+    target_rs = np.linspace(er.min(), er.max(), n_points)
+    weights = [minimize_vol(target_return, er, cov) for target_return in target_rs]
+    return weights
+
+
+def plot_ef(n_points, er, cov):
+    """
+    Plots the multi-asset efficient frontier
+    """
+    weights = optimal_weights(n_points, er, cov)
+    rets = [portfolio_return(w, er) for w in weights]
+    vols = [portfolio_vol(w, cov) for w in weights]
+    ef = pd.DataFrame({
+        "Returns": rets, 
+        "Volatility": vols
+    })
+    return ef.plot.line(x="Volatility", y="Returns", style='.-', legend=False)
